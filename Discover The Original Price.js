9614dd60f486d7434ae387c368d3d27aeae1e33e
JuliaVal821 <julia.kolgatina@gmail.com>
2020-08-20 16:15:49 -0400

    Wrote code to return the original product price and rounded to decimal.

---
+++ Discover The Original Price.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/552564a82142d701f5001228/train/javascript
+
+    function discoverOriginalPrice(discountedPrice, salePercentage){
+
+        return + (((discountedPrice * 100) / (100 - salePercentage)).toFixed(2)) ;
+    }
